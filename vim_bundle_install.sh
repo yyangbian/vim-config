b9c600a631cd766e6c9d1f53bfc8aeb8fbdd5cbc
Yang Yang <yyangbian@gmail.com>
2014-11-28 10:12:59 -0600

    added script to install related packages from github

---
+++ vim_bundle_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+git clone https://github.com/yyangbian/vim-config.git
+git clone https://github.com/yyangbian/vim-config.git
+git clone https://github.com/yyangbian/vim-config.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/Raimondi/delimitMate.git
+git clone https://github.com/davidhalter/jedi-vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/honza/vim-snippets.git
+git clone https://github.com/SirVer/ultisnips.git
+git clone https://github.com/kien/rainbow_parentheses.vim.git
